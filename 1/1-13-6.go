7db514a7a5f3bb5e113431ad282e351c3e28bccb
Kairat <suiunalievk@gmail.com>
2022-03-15 01:39:34 +0300

    Solved 1-13 partially

--- 1/1-13-6.go
+++ 1/1-13-6.go
@@ -3,12 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var a, b int
+	var a, b float32
 	fmt.Scan(&a, &b)
-
-	if ((a+b)/2)*10%10 == 0 {
-		fmt.Printf("%d", (a+b)/2)
-	} else {
-		fmt.Printf("%.1f", float32(a+b)/2)
-	}
+	fmt.Println((a + b) / 2)
 }
