d23fa4b357978efcb861df785d3964278359bdc8
Kairat <suiunalievk@gmail.com>
2022-03-20 21:59:08 +0300

    Solved till 3-1-6

--- 3/3-1-5.go
+++ 3/3-1-5.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func work(x int) int {
+	return x
+}
+
+func main() {
+	var a int
+	var m = make(map[int]int, 10)
+	for i := 0; i < 10; i++ {
+		fmt.Scan(&a)
+		_, ok := m[a]
+		if !ok {
+			m[a] = work(a)
+		}
+		fmt.Printf("%d ", m[a])
+	}
 }
